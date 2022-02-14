eea38827b9324cd85b3de60ef24bb37bf35d2fed
« marcillyl » <marcillyl@icloud.com>
2022-02-14 07:05:02 -0800

    Setup login component

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm!: FormGroup;
+  loading!: boolean;
+  errorMsg!: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required],
+    });
+  }
+  onLogin() {
+    this.loading = true;
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.auth
+      .loginUser(email, password)
+      .then(() => {
+        this.loading = false;
+        this.router.navigate(['/projects']);
+      })
+      .catch((error: { message: string }) => {
+        this.loading = false;
+        this.errorMsg = error.message;
+      });
+  }
+}
